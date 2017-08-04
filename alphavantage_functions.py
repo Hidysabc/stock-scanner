030b3e2190ce88487387e8510b179086a1dd3d23
Hidy Chiu <hidy0503@gmail.com>
2017-08-04 13:37:39 -0400

    Add Aplhavantage API modules to replace ystockquote functions

---
+++ alphavantage_functions.py
@@ -0,0 +1,63 @@
+import numpy as np
+import json
+import requests
+import datetime
+import pandas as pd
+
+f_apikey = '/workspace/practices/apikey.json'
+
+def get_apikey(f):
+    #reading apikey.json
+    apikey = list(json.load(open(f,"r")).values())[-1]
+    '''
+    this code is for reading apikey.txt
+    with open(f,'r') as ff:
+        apikey= ff.read().strip()
+    '''
+    return apikey
+
+
+def query_time_series_intraday(symbol,interval='1min',outputsize= 'compact',datatype='json'):
+    apikey = get_apikey(f_apikey)
+    request = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+symbol+'&interval='+interval+'&datatype='+datatype+'&outputsize='+outputsize+'&apikey='+apikey
+    items = requests.get(request).json()
+    return items
+
+def query_time_series_daily(symbol,outputsize= 'compact',datatype='json'):
+    apikey = get_apikey(f_apikey)
+    request = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&datatype='+datatype+'&outputsize='+outputsize+'&apikey='+apikey
+    items = requests.get(request).json()
+    return items
+
+
+def get_historical_prices(symbol,end_date,start_date):
+    #stocks = json.load(open(f,"r"))
+    stocks = query_time_series_daily(symbol)
+    k = list(stocks.keys())
+    stocks = stocks[k[-1]]
+    dates = list(stocks.keys())
+    start_date_parse = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date_parse = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+    span = int((end_date_parse - start_date_parse)/datetime.timedelta(1))
+    dateindex = pd.date_range(start_date,periods = span, freq = "D")
+    close = np.array([float(stocks[i]["4. close"]) for i in dateindex.astype(str) if i in dates])
+    #close = np.array([float(stocks[i]["4. close"]) for i in stocks])
+    #start_date_parse = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+    #end_date_parse = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+    #span = int((end_date_parse - start_date_parse)/datetime.timedelta(1))
+    #dateindex = pd.date_range(start_date,periods = span, freq = "D")
+    #close = [close[i] for i in dateindex.astype(str) if i in dates]
+    #close = close[dates.index(end_date):dates.index(start_date)]
+    #print(close)
+    return close
+
+def get_price(symbol):
+    #stocks = json.load(open(f,"r"))
+    stocks = query_time_series_intraday(symbol)
+    k = list(stocks.keys())
+    stocks = stocks[k[-1]]
+    dates = list(stocks.keys())
+    current = stocks[dates[0]]["4. close"]
+    #print(current)
+
+    return current
